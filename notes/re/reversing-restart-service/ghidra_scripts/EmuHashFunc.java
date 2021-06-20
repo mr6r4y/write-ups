b2914ada9abdc6c5221df93e643bac6f8b9f584c
mr6r4y <mr6r4y@gmail.com>
2021-06-20 20:19:05 +0300

    Fix

--- notes/re/reversing-restart-service/ghidra_scripts/EmuHashFunc.java
+++ notes/re/reversing-restart-service/ghidra_scripts/EmuHashFunc.java
@@ -10,7 +10,6 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.lang3.tuple.Triple;
@@ -19,6 +18,10 @@ import org.apache.commons.lang3.tuple.ImmutableTriple;
 import ghidra.app.emulator.EmulatorHelper;
 import ghidra.app.script.GhidraScript;
 import ghidra.program.model.address.Address;
+import ghidra.program.model.lang.Register;
+import ghidra.program.model.listing.Instruction;
+import ghidra.program.model.listing.Listing;
+import ghidra.program.model.mem.MemoryAccessException;
 import ghidra.program.model.symbol.*;
 import ghidra.util.Msg;
 import ghidra.util.exception.CancelledException;
@@ -33,6 +36,8 @@ public class EmuHashFunc extends GhidraScript {
 	private long stack_offset = 0x000000002FFF0000L;
 	private long stack_used = 0x10000L;
 	private long str_offset = 0x0;
+	private long current_stack = stack_offset - stack_used;
+	private long end_address_offset = 0x00405b8;
 	
 	private EmulatorHelper emu;
 	private Address do_some_hash_addr;
@@ -52,22 +57,19 @@ public class EmuHashFunc extends GhidraScript {
 	}
 	
 	protected void emuInit() throws NotFoundException {
-		Address current_stack = toAddr(stack_offset - stack_used);
-		Address end_address = toAddr(0x00405b8);
 		do_some_hash_addr = getSymbolAddress(hash_func_name);
-		
 		emu = new EmulatorHelper(currentProgram);
 		
-		emu.setBreakpoint(end_address);
-		emu.writeRegister(emu.getStackPointerRegister(), current_stack.getOffset());
-		emu.writeRegister("RBP", current_stack.getOffset());
-		emu.writeRegister(emu.getPCRegister(), do_some_hash_addr.getOffset());
+		emu.setBreakpoint(toAddr(end_address_offset));
 	}
 
 	protected long doSomeHash(String str, long hash) {
+		emu.writeRegister(emu.getStackPointerRegister(), current_stack);
+		emu.writeRegister("RBP", current_stack);
+		emu.writeRegister(emu.getPCRegister(), do_some_hash_addr.getOffset());
+		
 		Address str_mem = toAddr(stack_offset - str_offset);
 		byte[] str_bytes = str2bytes(str);
-		println(Arrays.toString(str_bytes));
 		emu.writeMemory(str_mem,  str_bytes);
 		
 		emu.writeRegister("RCX", str_mem.getOffset());
@@ -84,22 +86,42 @@ public class EmuHashFunc extends GhidraScript {
 		return rax;
 	}
 
-	protected boolean checkDllFuncName(String dll_name, String func_name, long dll_func_hash, long key) {
+	protected Long calckDllFuncNameHash(String dll_name, String func_name, long key) {
 		String dll_name_lower = dll_name.toLowerCase();
 		long dll_h = doSomeHash(dll_name_lower, key);
 		long func_h = doSomeHash(func_name, key);
+//		printf("dll: %s, func: %s, dll_func_hash: %X, key: %X\n", dll_name_lower, func_name, dll_h ^ func_h, key);
+		return (dll_h ^ func_h);
+	}
+	
+	private Long getRegRefFromCallAddr(Address call_addr, Listing l, String reg_name) {
+		Instruction ins = l.getInstructionBefore(call_addr);
+		for(byte i = 5; i > 0; i--) {
+			Object[] op_ar = ins.getOpObjects(0);
+			if (op_ar.length == 1) {
+				if (op_ar[0] instanceof Register) {
+					Register op = (Register) op_ar[0];
+					if (op.getName().compareTo(reg_name) == 0) {
+						for (Reference ref: ins.getReferencesFrom()) {
+							try {
+								return getLong(ref.getToAddress());
+							} catch (MemoryAccessException e) {}
+						}
+					}
+				}
+			}
+			ins = ins.getPrevious();
+		}
 		
-		return (dll_h ^ func_h) == dll_func_hash;
-	}
-	
-	private Long getDllFuncHashFromCallRef(Reference ref) {
-		// TO-DO: Implement backward propagation to references to data if any in RCX register
 		return 0L;
 	}
 	
-	private Long getKeyFromCallRef(Reference ref) {
-		// TO-DO: Implement backward propagation to references to data if any in RDX register
-		return 0L;
+	private Long getDllFuncHashFromCallAddr(Address call_addr, Listing l) {
+		return getRegRefFromCallAddr(call_addr, l, "RDX");
+	}
+
+	private Long getKeyFromCallAddr(Address call_addr, Listing l) {
+		return getRegRefFromCallAddr(call_addr, l, "R8");
 	}
 
 	protected List<Triple<Long, Long, Address>> collectDllHashes() throws NotFoundException {
@@ -109,12 +131,16 @@ public class EmuHashFunc extends GhidraScript {
 		ReferenceManager r = currentProgram.getReferenceManager();
 		ReferenceIterator references = r.getReferencesTo(f_addr);
 		
+		Listing l = currentProgram.getListing();
+		
 		for(Reference ref: references) {
-			Long h = getDllFuncHashFromCallRef(ref);
-			Long k = getKeyFromCallRef(ref);
 			Address a = ref.getFromAddress();
-			Triple<Long, Long, Address> t = new ImmutableTriple<Long, Long, Address>(h, k, a);
-			res.add(t);
+			Long h = getDllFuncHashFromCallAddr(a, l);
+			Long k = getKeyFromCallAddr(a, l);
+			if (k != 0 && h != 0) {
+				Triple<Long, Long, Address> t = new ImmutableTriple<Long, Long, Address>(h, k, a);
+				res.add(t);
+			}
 		}
 		
 		return res;
@@ -125,7 +151,7 @@ public class EmuHashFunc extends GhidraScript {
 		File names_fl;
 
 		try {
-			names_fl = askFile("Supply a file with <dll-name>:<func-name> list to be checked", getCategory());
+			names_fl = askFile("Supply a file with <dll-name>:<func-name> list to be checked", "Check");
 		} catch (CancelledException e) {
 			return;
 		}
@@ -137,6 +163,12 @@ public class EmuHashFunc extends GhidraScript {
 			return;
 		}
 		
+		println("--------- Refs ---------");
+		for(Triple<Long, Long, Address> p: dll_hashes) {
+			printf("address: %s, dll_func_hash: %X, key: %X\n", p.getRight(), p.getLeft(), p.getMiddle());
+		}
+		println("------------------------");
+		
 		try {
 			emuInit();
 		} catch (NotFoundException e) {
@@ -146,11 +178,25 @@ public class EmuHashFunc extends GhidraScript {
 		String line;
 		FileReader nr = new FileReader(names_fl.getAbsolutePath());
 		BufferedReader br = new BufferedReader(nr);
+		
 		while((line = br.readLine()) != null) {
+			String[] s = line.split(":");
+			Long current_hash = 0L;
+			Long last_key = 0L;
 			for(Triple<Long, Long, Address> p: dll_hashes) {
-				String[] s = line.split(":");
-				if(checkDllFuncName(s[0], s[1], p.getLeft(), p.getMiddle())) {
+				if ((p.getMiddle().compareTo(last_key) != 0) || (current_hash.equals(0L))) {
+					last_key = p.getMiddle();
+					current_hash = calckDllFuncNameHash(s[0], s[1], last_key);
+				}
+				
+				if (current_hash == p.getLeft() ) {
 					printf("name: %s, dll_func_hash: %X, key: %X\n", line, p.getLeft(), p.getMiddle());
+				}
+				
+				if (monitor.isCancelled()) {
+					br.close();
+					emu.dispose();
+					return;
 				}
 			}
 		}
